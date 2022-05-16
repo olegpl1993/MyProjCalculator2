b04b853c2e83286f0d9c5df8863981d7fc335cb5
olegpl1993 <79967092+olegpl1993@users.noreply.github.com>
2022-05-16 16:54:58 +0300

    Добавил темную тему, пофиксил баги

--- js/script.js
+++ js/script.js
@@ -1,57 +1,142 @@
 'use strict'
 
-num1.addEventListener("click", function () {
-    inputStr.textContent += num1.textContent;
-});
-num2.addEventListener("click", function () {
-    inputStr.textContent += num2.textContent;
-});
-num3.addEventListener("click", function () {
-    inputStr.textContent += num3.textContent;
-});
-num4.addEventListener("click", function () {
-    inputStr.textContent += num4.textContent;
-});
-num5.addEventListener("click", function () {
-    inputStr.textContent += num5.textContent;
-});
-num6.addEventListener("click", function () {
-    inputStr.textContent += num6.textContent;
-});
-num7.addEventListener("click", function () {
-    inputStr.textContent += num7.textContent;
-});
-num8.addEventListener("click", function () {
-    inputStr.textContent += num8.textContent;
-});
-num9.addEventListener("click", function () {
-    inputStr.textContent += num9.textContent;
-});
-num0.addEventListener("click", function () {
-    inputStr.textContent += num0.textContent;
-});
-divide.addEventListener("click", function () {
-    inputStr.textContent += "/";
-});
-mult.addEventListener("click", function () {
-    inputStr.textContent += "*";
-});
-minus.addEventListener("click", function () {
-    inputStr.textContent += "-";
-});
-plus.addEventListener("click", function () {
-    inputStr.textContent += "+";
-});
-point.addEventListener("click", function () {
-    inputStr.textContent += ".";
-});
-
-clear.addEventListener("click", function () {
-    inputStr.textContent = "";
-});
-count.addEventListener("click", function () {
+//подсчитывает результат в строке 
+function counting() {
     let string = inputStr.textContent;
     inputStr.textContent = eval(string);
+}
+
+//переносит результат из основной строки в дополнительную
+function transfer() {
+    backString.textContent = inputStr.textContent;
+}
+
+num1.addEventListener("click", function () {
+    if (inputStr.textContent.length < 25) //проверка на длину строки
+    {
+        inputStr.textContent += "1";
+    }
+});
+num2.addEventListener("click", function () {
+    if (inputStr.textContent.length < 25) {
+        inputStr.textContent += "2";
+    }
+});
+num3.addEventListener("click", function () {
+    if (inputStr.textContent.length < 25) {
+        inputStr.textContent += "3";
+    }
+});
+num4.addEventListener("click", function () {
+    if (inputStr.textContent.length < 25) {
+        inputStr.textContent += "4";
+    }
+});
+num5.addEventListener("click", function () {
+    if (inputStr.textContent.length < 25) {
+        inputStr.textContent += "5";
+    }
+});
+num6.addEventListener("click", function () {
+    if (inputStr.textContent.length < 25) {
+        inputStr.textContent += "6";
+    }
+});
+num7.addEventListener("click", function () {
+    if (inputStr.textContent.length < 25) {
+        inputStr.textContent += "7";
+    }
+});
+num8.addEventListener("click", function () {
+    if (inputStr.textContent.length < 25) {
+        inputStr.textContent += "8";
+    }
+});
+num9.addEventListener("click", function () {
+    if (inputStr.textContent.length < 25) {
+        inputStr.textContent += "9";
+    }
+});
+num0.addEventListener("click", function () {
+    if (inputStr.textContent.length < 25) {
+        inputStr.textContent += "0";
+    }
+});
+divide.addEventListener("click", function () {
+    if (inputStr.textContent.length < 25) {
+        inputStr.textContent += "/";
+    }
+});
+mult.addEventListener("click", function () {
+    if (inputStr.textContent.length < 25) {
+        inputStr.textContent += "*";
+    }
+});
+minus.addEventListener("click", function () {
+    if (inputStr.textContent.length < 25) {
+        inputStr.textContent += "-";
+    }
+});
+plus.addEventListener("click", function () {
+    if (inputStr.textContent.length < 25) {
+        inputStr.textContent += "+";
+    }
+});
+point.addEventListener("click", function () {
+    if (inputStr.textContent.length < 25) {
+        inputStr.textContent += ".";
+    }
+});
+eraser.addEventListener("click", function () {
+    let str = inputStr.textContent;
+    let str2 = str.substring(0, str.length - 1); //уменьшает строку на 1 символ
+    inputStr.textContent = str2;
+});
+clear.addEventListener("click", function () {
+    inputStr.textContent = "";
+    backString.textContent = "";
+});
+count.addEventListener("click", function () {
+    transfer();
+    counting();
 });
 
+changeColorBlack.addEventListener("click", function () {
+    //цвет цифровой панели
+    document.querySelector(".input__backString").style.backgroundColor = "#303030";
+    document.querySelector(".input__strind").style.backgroundColor = "#303030";
+    document.querySelector(".input__backString").style.color = "white";
+    document.querySelector(".input__strind").style.color = "white";
+    //цвет коробки
+    document.querySelector(".box").style.borderColor = "white";
+    document.querySelector(".box").style.backgroundColor = "#5c5c5c";
+    //цвет обертки
+    document.querySelector(".wrapper").style.backgroundColor = "black";
+    //цвет кнопок
+    const butt = document.querySelectorAll(".buttons__butt");
+    for (let i = 0; i < butt.length; i++) {
+        butt[i].style.color = "white";
+        butt[i].style.backgroundColor = "#303030";
+        butt[i].style.borderColor = "white";
+    }
+});
 
+changeColorWhite.addEventListener("click", function () {
+    //цвет цифровой панели
+    document.querySelector(".input__backString").style.backgroundColor = "#ebebeb";
+    document.querySelector(".input__strind").style.backgroundColor = "#ebebeb";
+    document.querySelector(".input__backString").style.color = "black";
+    document.querySelector(".input__strind").style.color = "black";
+    //цвет коробки
+    document.querySelector(".box").style.borderColor = "black";
+    document.querySelector(".box").style.backgroundColor = "#e4e4e4";
+    //цвет обертки
+    document.querySelector(".wrapper").style.backgroundColor = "white";
+    //цвет кнопок 
+    const butt = document.querySelectorAll(".buttons__butt");
+    for (let i = 0; i < butt.length; i++) {
+        butt[i].style.color = "black";
+        butt[i].style.backgroundColor = "#cccccc";
+        butt[i].style.borderColor = "black";
+    }
+});
